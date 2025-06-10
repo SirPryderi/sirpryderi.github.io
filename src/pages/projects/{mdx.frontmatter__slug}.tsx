2af81ad4f1f1e4840f42bf0d19349aaa37c495e1
Vittorio Iocolano <pryderi.mail@gmail.com>
2025-06-10 18:22:35 +0100

    Use layout in projects

--- src/pages/projects/{mdx.frontmatter__slug}.tsx
+++ src/pages/projects/{mdx.frontmatter__slug}.tsx
@@ -1,6 +1,7 @@
 import * as React from "react"
 import { graphql } from "gatsby"
 import { MDXProvider } from "@mdx-js/react"
+import Layout from "../../components/Layout"
 
 type Props = {
   data: Queries.ProjectQuery
@@ -8,17 +9,19 @@ type Props = {
 }
 
 export default function ProjectTemplate({ data, children }: Props) {
-  const frontmatter  = data.mdx?.frontmatter
+  const frontmatter = data.mdx?.frontmatter
   return (
-    <article>
-      <h1>{frontmatter?.name}</h1>
-      <MDXProvider>{children}</MDXProvider>
-    </article>
+    <Layout>
+      <article>
+        <h1>{frontmatter?.name}</h1>
+        <MDXProvider>{children}</MDXProvider>
+      </article>
+    </Layout>
   )
 }
 
 export const pageQuery = graphql`
-  query Project ($id: String!) {
+  query Project($id: String!) {
     mdx(id: { eq: $id }) {
       frontmatter {
         slug
