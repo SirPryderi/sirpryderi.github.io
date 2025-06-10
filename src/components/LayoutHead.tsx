834c6c2bf0b258c3efe5f1f4665fe5150271bd6c
Vittorio Iocolano <pryderi.mail@gmail.com>
2025-06-10 19:46:26 +0100

    Add style for project

---
+++ src/components/LayoutHead.tsx
@@ -0,0 +1,24 @@
+import * as React from "react"
+import "../cosmos/scene"
+import { HeadFC } from "gatsby"
+
+type PageContext = { frontmatter: { name: string } }
+
+const LayoutHead: HeadFC<object, PageContext> = ({ pageContext }) => (
+  <>
+    <title>{pageContext.frontmatter?.name || "Home Page"}</title>
+    <style>{`body { margin: 0; }`}</style>
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link
+      rel="preconnect"
+      href="https://fonts.gstatic.com"
+      crossOrigin="anonymous"
+    />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Oxanium:wght@200..800&display=swap"
+      rel="stylesheet"
+    />
+  </>
+)
+
+export default LayoutHead
--- src/pages/projects/{mdx.frontmatter__slug}.tsx
+++ src/pages/projects/{mdx.frontmatter__slug}.tsx
@@ -2,23 +2,50 @@ import * as React from "react"
 import { graphql } from "gatsby"
 import { MDXProvider } from "@mdx-js/react"
 import Layout from "../../components/Layout"
+import styled from "styled-components"
+import LayoutHead from "../../components/LayoutHead"
 
 type Props = {
   data: Queries.ProjectQuery
   children: React.ReactNode
 }
 
+const Article = styled.article`
+  max-width: 900px;
+  margin: 0 auto;
+  padding: 4rem 2rem 2rem 2rem;
+  color: white;
+  font-family: "Oxanium", sans-serif;
+
+  @media (max-width: 768px) {
+    padding: 2rem 1rem;
+  }
+`
+
+const ProjectTitle = styled.h1`
+  font-size: 3rem;
+  font-weight: 600;
+  margin-bottom: 2rem;
+  letter-spacing: 0.04em;
+
+  @media (max-width: 768px) {
+    font-size: 2rem;
+  }
+`
+
 export default function ProjectTemplate({ data, children }: Props) {
   const frontmatter = data.mdx?.frontmatter
   return (
     <Layout>
-      <article>
-        <h1>{frontmatter?.name}</h1>
+      <Article>
+        <ProjectTitle>{frontmatter?.name}</ProjectTitle>
         <MDXProvider>{children}</MDXProvider>
-      </article>
+      </Article>
     </Layout>
   )
 }
+
+export const Head = LayoutHead
 
 export const pageQuery = graphql`
   query Project($id: String!) {
