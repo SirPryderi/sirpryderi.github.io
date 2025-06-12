7c0fc864440ceee12e382f5ea8fc9e725b486a8f
Vittorio Iocolano <pryderi.mail@gmail.com>
2025-06-12 16:02:09 +0100

    Set theme color in meta tag

--- src/components/LayoutHead.tsx
+++ src/components/LayoutHead.tsx
@@ -7,6 +7,8 @@ const LayoutHead: HeadFC<object, PageContext> = ({ pageContext }) => (
   <>
     <title>{pageContext.frontmatter?.name || "Home Page"}</title>
     <style>{`body { margin: 0; background-color: #0a0e0c; }`}</style>
+    <meta name="color-scheme" content="dark" />
+    <meta name="theme-color" content="#0a0e0c" />
   </>
 )
 
